cd6c85febfd821bfeb659405726691ffa74bdf3a
Katherine Lin <kl2515@rit.edu>
2019-11-11 15:21:19 -0500

    Update msg-email.php

--- msg-email.php
+++ msg-email.php
@@ -16,31 +16,4 @@ $email = filter_email_header($email);
 $headers = "From: $email\n";
 $sent = mail('katherinelindev@gmail.com', 'Message Form Submission', $name, $message, $headers);
 
-# Thank user or notify them of a problem
-if ($sent) {
-
-?><html>
-<head>
-<title>Thank You</title>
-</head>
-<body>
-<h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
-</body>
-</html>
-<?php
-}
 ?>
